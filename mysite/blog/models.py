1c385af2bea60e9a56d6cf5c783ed13e23a64919
hari-testpress <hari@testpress.in>
2021-09-13 21:04:17 +0530

    Refactor code

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 from taggit.managers import TaggableManager
+from django.db.models import Count
 
 
 class PublishedManager(models.Manager):
@@ -53,6 +54,15 @@ class Post(models.Model):
                 self.slug,
             ],
         )
+
+    def get_top_four_similar_posts(self):
+        post_tag_ids = self.tags.values_list("id", flat=True)
+        similar_posts = Post.published.filter(tags__in=post_tag_ids).exclude(
+            id=self.id
+        )
+        return similar_posts.annotate(same_tags=Count("tags")).order_by(
+            "-same_tags", "-publish"
+        )[:4]
 
 
 class Comment(models.Model):
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -58,25 +58,17 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommentForm()
 
-    post_tags_ids = post.tags.values_list("id", flat=True)
-    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(
-        id=post.id
-    )
-    similar_posts = similar_posts.annotate(same_tags=Count("tags")).order_by(
-        "-same_tags", "-publish"
-    )[:4]
-
-    return render(
-        request,
-        "blog/post/detail.html",
-        {
-            "post": post,
-            "comments": comments,
-            "new_comment": new_comment,
-            "comment_form": comment_form,
-            "similar_posts": similar_posts,
-        },
-    )
+        return render(
+            request,
+            "blog/post/detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "new_comment": new_comment,
+                "comment_form": comment_form,
+                "similar_posts": post.get_top_four_similar_posts(),
+            },
+        )
 
 
 def post_share(request, post_id):
